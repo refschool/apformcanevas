a3a638c7d97156d06a82f3f3766ae9cd8cf3e983
Yvon Huynh <yvon.huynh@gmail.com>
2021-01-15 12:07:16 +0100

    category OK

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -35,7 +35,7 @@ class Category
     private $slug;
 
     /**
-     * @ORM\OneToMany(targetEntity=Product::class, mappedBy="category")
+     * @ORM\OneToMany(targetEntity=Product::class, mappedBy="category",cascade={"remove"}, orphanRemoval=true)
      */
     private $products;
 
